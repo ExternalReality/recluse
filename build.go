c1aa421e0e4fc45016c955682be2d0d3da6cfc42
externalreality <ecjones@protonmail.com>
2019-04-14 01:39:32 -0400

    Create build directory.

--- build.go
+++ build.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"os/user"
+
 	"github.com/spf13/cobra"
 )
 
@@ -8,9 +11,22 @@ var cmdBuild = &cobra.Command{
 	Use:   "build [binary]",
 	Short: "Build a Hermitux kernel for a given binary",
 	Args:  cobra.MinimumNArgs(1),
-	Run:   run,
+	Run:   build,
 }
 
 func build(cmd *cobra.Command, args []string) {
-
+	usr, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	dir := usr.HomeDir + "/.recluse"
+	_, err = os.Stat(dir)
+	if !os.IsNotExist(err) {
+		panic(err)
+	}
+	err = os.MkdirAll(dir+"/bin", os.ModePerm)
+	err = os.MkdirAll(dir+"/lib", os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
