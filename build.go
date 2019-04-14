349310b58f28372287162b49f115d0c05032cabb
externalreality <ecjones@protonmail.com>
2019-04-14 13:42:32 -0400

    Use docker get package build env.

--- build.go
+++ build.go
@@ -1,16 +1,64 @@
 package main
 
 import (
+	"io"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
 	"github.com/spf13/cobra"
+	"golang.org/x/net/context"
 )
+
+const buildEnvImg string = "rwthos/hermitcore"
 
 var cmdBuild = &cobra.Command{
 	Use:   "build [binary]",
 	Short: "Build a Hermitux kernel for a given binary",
 	Args:  cobra.MinimumNArgs(1),
-	Run:   run,
+	Run:   build,
 }
 
 func build(cmd *cobra.Command, args []string) {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.WithVersion("1.38"))
+	if err != nil {
+		panic(err)
+	}
 
+	reader, err := cli.ImagePull(ctx, buildEnvImg, types.ImagePullOptions{})
+	if err != nil {
+		panic(err)
+	}
+	io.Copy(os.Stdout, reader)
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: buildEnvImg,
+		Cmd:   []string{"echo", "In docker image"},
+		Tty:   true,
+	}, nil, nil, "")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
+
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			panic(err)
+		}
+	case <-statusCh:
+	}
+
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		panic(err)
+	}
+
+	io.Copy(os.Stdout, out)
 }
